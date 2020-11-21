bf5771aa2faa402e70e8e6eab3c4db350e60930e
Murad Sarker <sarker24@gmail.com>
2020-11-21 23:53:14 +0100

    Use resource generator for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_144133) do
+ActiveRecord::Schema.define(version: 2020_11_21_222321) do
+
+  create_table "posts", force: :cascade do |t|
+    t.date "date"
+    t.text "rational"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
