51f0e0b7233ad9c4ea38c0de3a63d496ca7cc9fc
Murad Sarker <sarker24@gmail.com>
2020-11-22 01:45:57 +0100

    created seed file with default post content

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_21_222321) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.date "date"
     t.text "rational"
